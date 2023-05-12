99a98d1b8fc678ce54c7f97657088d3569ec8298
Jovan Alejandro Zambrano Bello <75374943+a-zambrano1@users.noreply.github.com>
2023-05-12 13:30:54 -0500

    Update GestionMateriales

--- components/GestionMateriales.tsx
+++ components/GestionMateriales.tsx
@@ -1,14 +1,16 @@
+import React from 'react'
 
 const GestionMateriales = () => {
   return (
     <div>
-        <span>navbar</span>
-        <span>titulo</span>
-        <span>boton</span>
-        <span>tabla</span>
+      <span>navbar</span>
+      <span>titulo</span>
+      <span>boton</span>
+      <span>tabla</span>
+      <span>footer</span>
     </div>
   )
 }
 
-export {GestionMateriales}
+export default GestionMateriales
 
