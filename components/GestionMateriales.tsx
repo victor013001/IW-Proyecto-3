57fb736f9648c95379e25af9a010b3943cdbf391
Jovan Alejandro Zambrano Bello <75374943+a-zambrano1@users.noreply.github.com>
2023-05-12 13:08:00 -0500

    Inicio de modulo Materiales

---
+++ components/GestionMateriales.tsx
@@ -0,0 +1,14 @@
+
+const GestionMateriales = () => {
+  return (
+    <div>
+        <span>navbar</span>
+        <span>titulo</span>
+        <span>boton</span>
+        <span>tabla</span>
+    </div>
+  )
+}
+
+export {GestionMateriales}
+
