71acd1eb619eb46c97bca437285e4c5b771b0289
Jovan Alejandro Zambrano Bello <75374943+a-zambrano1@users.noreply.github.com>
2023-05-19 12:54:54 -0500

    Update del modulo de Materiales

---
+++ pages/materiales.tsx
@@ -0,0 +1,12 @@
+import GestionMateriales from '@components/GestionMateriales'
+import React from 'react'
+
+const materiales = () => {
+  return (
+    <div>
+      <GestionMateriales/>
+    </div>
+  )
+}
+
+export default materiales
