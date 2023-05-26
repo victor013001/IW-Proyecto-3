d7e9dac8ab1332bb675b7d57f467a572acfa68af
Jovan Alejandro Zambrano Bello <75374943+a-zambrano1@users.noreply.github.com>
2023-05-26 11:33:10 -0500

    Organización de la tabla del modulo materiales

--- components/GestionMateriales.tsx
+++ components/GestionMateriales.tsx
@@ -2,36 +2,38 @@ import React from 'react'
 
 const GestionMateriales = () => {
   return (
-    <div className='border flex flex-row '>
-      <div className='border w-[20%]'>
-        <span>navbar</span>  
-      </div>
-      <div className='border w-[80%] flex flex-col justify-between items-center'>
-        <div>
+    <div className='border flex flex-row h-[100%]'>
+      <div className='border w-[80%] flex flex-col justify-between items-center '>
+        <div className='m-10'>
           <span className='text-normal text-5xl'>Gestión de Materiales</span>
         </div>
-        <div className='border w-[100%] flex flex-col pl-24 pr-20'>
-          <button>boton</button>
-          <table>
-            <thead>ola</thead>
-            <tbody>
-              <tr>
-                <td>Identificación</td>
-              </tr>
-              <tr>
-                <td>Fecha de Creación</td>
-              </tr>
-              <tr>Nombre</tr>
-              <tr>Saldo</tr>
-            </tbody>
-
-          </table>
-        </div>
-        <div className='border h-[90px] flex flex-row justify-between w-[100%] pt-5 pl-24 pr-20'>
-          <span>Universidad de Antioquia</span>
-          <span>ConstruStock</span>
-          <span>Integrantes</span>
-          <span>Ingeniería Web</span>
+        <div className='border w-[100%] h-[100%] flex flex-col pl-24 pr-20'>
+          <div className='p-6 m-6 flex flex-col justify-center items-center'>
+          <table className='block'>
+              <thead>
+                <tr>
+                  <th>Identificador</th>
+                  <th>Fecha de Creación</th>
+                  <th>Nombre</th>
+                  <th>Saldo</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>12/12/2021</td>
+                  <td>Material 1</td>
+                  <td>100</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>12/12/2021</td>
+                  <td>Material 2</td>
+                  <td>100</td>
+                </tr>
+              </tbody>  
+            </table>
+          </div>
         </div>       
       </div>
     </div>  
