5ed01636e1a6ce99bbc5b2aa3443dea8f1c2d427
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-05-27 14:06:00 -0500

    Actualizacion con base de datos

--- components/modals/ModalMateriales.tsx
+++ components/modals/ModalMateriales.tsx
@@ -1,16 +1,45 @@
 import { useMaterialsContext } from '@context/materialesContext';
 import Modal from './Modal';
-import React, { FormEvent } from 'react'
+import React, { FormEvent, useState } from 'react'
+import { useMutation} from '@apollo/client';
+import { CREATE_MATERIAL } from 'graphql/client/material';
+import { toast } from 'react-toastify';
 
 
 
 const ModalMateriales = () => {
 
-  const {openModalMateriales, setOpenModalMateriales} = useMaterialsContext();
+	const [formData, setFormData] = useState<{
+		name: string;
+		quantity: number;
+	}>({
+		name: '',
+		quantity: 0,
+	});
 
-  const submitForm = (e:FormEvent<HTMLFormElement>) => {
+	const [createMaterial, {loading}] = useMutation(CREATE_MATERIAL);
+
+	if (loading) return <div>Creando...</div>
+	
+
+  	const {openModalMateriales, setOpenModalMateriales} = useMaterialsContext();
+
+  	const submitForm = (e:FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 	};
+
+	try{
+		const {} =createMaterial({
+			variables: {
+				name: formData.name,
+				input: formData.quantity,
+			},
+		 });
+		toast.success('Material creado exitosamente');
+		setOpenModalMateriales(false);	
+		}catch(error){
+			toast.error('Error al crear el material');
+		}
 
   return (
     <Modal
@@ -21,11 +50,34 @@ const ModalMateriales = () => {
 			<form onSubmit={submitForm} className='flex flex-col gap-3'>
 				<label htmlFor='name' className='flex flex-col'>
 					<span>Nombre</span>
-					<input type='text' name='name'></input>
+					<input id='names'
+					type='text'
+					name='names'
+					value={formData.name.toString()}
+					onChange={(e) =>
+						setFormData((prev) => ({
+							...prev,
+							name: e.target.value,
+						}))
+					}
+					/>
 				</label>
 				<label htmlFor='balance' className='flex flex-col'>
 					<span>Cantidad</span>
-					<input type='number' name='balance' min={0} step={1} placeholder='0'></input>
+					<input
+					type='number' 
+					name='balance' 
+					min={0}
+					step={1}
+					placeholder='0'
+					value={formData.quantity.toString()}
+					onChange={(e) =>
+						setFormData((prev) => ({
+							...prev,
+							quantity: parseInt(e.target.value),
+						}))
+					}
+					/>
 				</label>
 				<div className='w-full flex justify-center gap-6'>
 						<button type='button' onClick={()=>setOpenModalMateriales(false)} className='secondary'>Cancelar</button>
@@ -35,6 +87,6 @@ const ModalMateriales = () => {
 		</div>
     </Modal>
   )
-}
+}	
 
 export {ModalMateriales}
