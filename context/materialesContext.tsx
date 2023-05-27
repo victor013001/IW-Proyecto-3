cd6eab8cf7cf6c5fa3b4ac30a459ee3d69f09a6e
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-05-27 11:04:49 -0500

    Creacion del contexto de materiales

---
+++ context/materialesContext.tsx
@@ -0,0 +1,31 @@
+import { Dispatch, SetStateAction, createContext, useContext, useState } from "react";
+
+interface MaterialesContextProps {
+	openModalMateriales: boolean;
+	setOpenModalMovimientos: Dispatch<SetStateAction<boolean>>;
+}
+
+const MaterialesContext = createContext<MaterialesContextProps>(
+  {} as MaterialesContextProps
+);
+
+export const useMaterialsContext = () => useContext(MaterialesContext);
+
+interface MaterialesContextProviderProps {
+	children: JSX.Element;
+}
+
+const MaterialesContextProvider = ({children}:MaterialesContextProviderProps) =>{
+	const [openModalMateriales, setOpenModalMateriales] = useState<boolean>(false);
+
+	return (
+		<MaterialesContext.Provider 
+			value={{openModalMateriales, setOpenModalMateriales}}
+		>
+			{children}
+		</MaterialesContext.Provider>
+	);
+	
+};
+
+export {MaterialesContextProvider}
