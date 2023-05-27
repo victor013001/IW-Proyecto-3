bf12f21847e2e909cd70a10b6ae746542a532e97
Victor Manuel Osorio Garcia <victormanuelosoriog@gmail.com>
2023-05-27 17:52:18 -0500

    typescript suggestion

--- components/GestionMateriales.tsx
+++ components/GestionMateriales.tsx
@@ -1,40 +1,34 @@
 import { useQuery } from '@apollo/client';
-import { GET_MATERIAL, GET_MATERIALS_BALANCE } from 'graphql/client/material';
-import React from 'react'
+import { GET_MATERIALS_BALANCE } from 'graphql/client/material';
+import React from 'react';
 import { MaterialBalance } from 'types';
 import { AgregarMaterial } from './AgregarMaterial';
 import { ModalMateriales } from './modals/ModalMateriales';
 import { Loading } from './Loading';
 
-
 const GestionMateriales = () => {
 
-  interface Material{
-    id: string;
-    name: string;
-  }
-
-    const {data, loading} =
-    useQuery<{materials: MaterialBalance[]}>(GET_MATERIALS_BALANCE,
+  const { data, loading } = useQuery<{ materials: MaterialBalance[] }>(
+    GET_MATERIALS_BALANCE,
     {
-      fetchPolicy:'network-only',
-    });
-  if (loading) return <Loading/>;
-
+      fetchPolicy: 'network-only',
+    }
+  );
+  if (loading) return <Loading />;
 
   return (
     <div className='flex h-full w-full flex-col p-4 gap-2'>
-      <div>
+      <div className='flex h-full w-full flex-col gap-3'>
         <div className='flex justify-center'>
           <h1>Gestión de Materiales</h1>
         </div>
         <div className='flex justify-between'>
-          <AgregarMaterial/>
-          <ModalMateriales/>    
+          <AgregarMaterial />
+          <ModalMateriales />
         </div>
         <div className='h-full flex flex-col'>
           <div className='h-[80vh] flex justify-center p-6 overflow-y-auto'>
-          <table className='block'>
+            <table className='block'>
               <thead>
                 <tr>
                   <th>Identificador</th>
@@ -45,35 +39,30 @@ const GestionMateriales = () => {
               </thead>
               <tbody>
                 {data?.materials?.map((material) => {
-                  return(
+                  return (
                     <tr key={`row_${material.id}`}>
                       <td>
-                        <div>{material.id}</div>  
+                        <div>{material.id}</div>
                       </td>
                       <td>
-                        <div>{material.createdAt}</div> 
+                        <div>{material.createdAt}</div>
                       </td>
                       <td>
-                        <div>{material.name}</div> 
+                        <div>{material.name}</div>
                       </td>
                       <td>
                         <div>{material.balance}</div>
-                      </td>     
+                      </td>
                     </tr>
-                  )
+                  );
                 })}
-              </tbody>  
+              </tbody>
             </table>
           </div>
-        </div>       
+        </div>
       </div>
-    </div>  
-  )
-}
+    </div>
+  );
+};
 
-export default GestionMateriales
-
-function useMaterialesContext(): { setOpenModalMateriales: any; } {
-  throw new Error('Function not implemented.');
-}
-
+export default GestionMateriales;
--- components/modals/ModalMateriales.tsx
+++ components/modals/ModalMateriales.tsx
@@ -1,109 +1,109 @@
 import { useMaterialsContext } from '@context/materialesContext';
 import Modal from './Modal';
-import React, { FormEvent, useState } from 'react'
-import { useMutation} from '@apollo/client';
+import React, { FormEvent, useState } from 'react';
+import { useMutation } from '@apollo/client';
 import { CREATE_MATERIAL } from 'graphql/client/material';
 import { toast } from 'react-toastify';
-import { create } from 'lodash';
-
-
 
 const ModalMateriales = () => {
+  const [formData, setFormData] = useState<{
+    name: string;
+    quantity: number;
+  }>({
+    name: '',
+    quantity: 0,
+  });
 
-	const [formData, setFormData] = useState<{
-		name: string;
-		quantity: number;
-	}>({
-		name: '',
-		quantity: 0,
-	});
+  const [createMaterial, { data, loading }] = useMutation(CREATE_MATERIAL);
+  const { openModalMateriales, setOpenModalMateriales } = useMaterialsContext();
 
-	const [createMaterial, {data, loading}] = useMutation(CREATE_MATERIAL);
+  if (loading) return <div>Creando...</div>;
 
-	if (loading) return <div>Creando...</div>;
-	
+  const submitForm = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await createMaterial({
+        variables: {
+          name: formData.name,
+          input: formData.quantity,
+        },
+      });
 
-  	const {openModalMateriales, setOpenModalMateriales} = useMaterialsContext();
-
-  	const submitForm = async (e:FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		try{
-			await createMaterial({
-				variables: {
-					name: formData.name,
-					input: formData.quantity,
-				},
-			 });
-
-			if (data?.data?.createMaterial === null){
-				toast.warning('Ya existe un material con ese nombre');
-				return;
-			} 
-			toast.success('Material creado exitosamente');
-			setFormData({
-				name: '',
-				quantity: 0,
-			});
-			setOpenModalMateriales(false);	
-		}catch(error){
-			toast.error('Error al crear el material');
-		}
-	};
-
-	
+      if (data?.data?.createMaterial === null) {
+        toast.warning('Ya existe un material con ese nombre');
+        return;
+      }
+      toast.success('Material creado exitosamente');
+      setFormData({
+        name: '',
+        quantity: 0,
+      });
+      setOpenModalMateriales(false);
+    } catch (error) {
+      toast.error('Error al crear el material');
+    }
+  };
 
   return (
     <Modal
-	open={openModalMateriales}
-	setOpen={setOpenModalMateriales}
-	modalTitle='Agregar nuevo material'>
-        <div>
-			{loading ? (
-				<div>Creando...</div>
-			): (
-				<form onSubmit={submitForm} className='flex flex-col gap-3'>
-				<label htmlFor='name' className='flex flex-col'>
-					<span>Nombre</span>
-					<input id='names'
-					required
-					type='text'
-					name='names'
-					value={formData.name.toString()}
-					onChange={(e) =>
-						setFormData((prev) => ({
-							...prev,
-							name: e.target.value,
-						}))
-					}
-					/>
-				</label>
-				<label htmlFor='balance' className='flex flex-col'>
-					<span>Cantidad</span>
-					<input
-					type='number'
-					required 
-					name='balance' 
-					min={1}
-					step={1}
-					placeholder='0'
-					value={formData.quantity.toString()}
-					onChange={(e) =>
-						setFormData((prev) => ({
-							...prev,
-							quantity: parseInt(e.target.value),
-						}))
-					}
-					/>
-				</label>
-				<div className='w-full flex justify-center gap-6'>
-						<button type='button' onClick={()=>setOpenModalMateriales(false)} className='secondary'>Cancelar</button>
-						<button type='submit'>Crear</button>
-				</div>
-			</form>
-			)}				
-		</div>
+      open={openModalMateriales}
+      setOpen={setOpenModalMateriales}
+      modalTitle='Agregar nuevo material'
+    >
+      <div>
+        {loading ? (
+          <div>Creando...</div>
+        ) : (
+          <form onSubmit={submitForm} className='flex flex-col gap-3'>
+            <label htmlFor='name' className='flex flex-col'>
+              <span>Nombre</span>
+              <input
+                id='names'
+                required
+                type='text'
+                name='names'
+                value={formData.name.toString()}
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    name: e.target.value,
+                  }))
+                }
+              />
+            </label>
+            <label htmlFor='balance' className='flex flex-col'>
+              <span>Cantidad</span>
+              <input
+                type='number'
+                required
+                name='balance'
+                min={1}
+                step={1}
+                placeholder='0'
+                value={formData.quantity.toString()}
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    quantity: parseInt(e.target.value),
+                  }))
+                }
+              />
+            </label>
+            <div className='w-full flex justify-center gap-6'>
+              <button
+                type='button'
+                onClick={() => setOpenModalMateriales(false)}
+                className='secondary'
+              >
+                Cancelar
+              </button>
+              <button type='submit'>Crear</button>
+            </div>
+          </form>
+        )}
+      </div>
     </Modal>
-  )
-}	
+  );
+};
 
-export {ModalMateriales}
+export { ModalMateriales };
