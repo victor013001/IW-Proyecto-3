9acb5c1b19a8f3ed3fd084a1b181cf626fd12aff
arleyberrio <arleyberrioarroyave@gmail.com>
2023-05-28 13:32:12 -0500

    boton de redireccionar a inventarios una vez iniciada sesión

--- pages/HeaderWithLogin.tsx
+++ pages/HeaderWithLogin.tsx
@@ -1,23 +1,16 @@
-import { signIn } from 'next-auth/react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { MdFacebook, MdWhatsapp } from 'react-icons/md';
 
-const Header = () => {
-   const router = useRouter();
+const HeaderWithLogin = () => {
     return(
+        <div className='body-landing h-screen w-full'>
     <header className='flex  w-full'>
         <div className='flex  justify-between w-full mt-6'>
         <div className='mt-3 mr-24 mb-3 ml-10'>
             <span className='text-white font-bold text-4xl'>ConstruStock</span>
         </div>
         <div className='flex gap-9 mt-3 mr-24 mb-3'>
-            <Link
-            href='@components/HeaderWithLogin'
-            className='bg-color-background-buttom p-2  text-white font-semibold rounded-md hover:cursor-pointer hover:scale-105'
-            >
-            Ir a la seccion de inventarios
-            </Link>
+         
             <div>
             <a
                 href='https://www.facebook.com/'
@@ -37,6 +30,15 @@ const Header = () => {
         </div>
         </div>
     </header>
+    <div className='flex w-full  justify-center align-middle'>
+        <Link
+        href='/inventarios'
+        className='bg-color-background-buttom p-2  text-white font-semibold rounded-md hover:cursor-pointer hover:scale-105'
+        >
+            Ir a la seccion de inventarios
+        </Link>
+    </div>
+    </div>
   )};
 
-export { Header };
+export default HeaderWithLogin;
